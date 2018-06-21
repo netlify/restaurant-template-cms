1dfdac93a4667ca7562812380fb043e476535628
Irene Morente <imorente@gmail.com>
2018-06-21 12:43:46 -0700

    Fix build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,7 @@ import cp from "child_process";
 import gutil from "gulp-util";
 import postcss from "gulp-postcss";
 import cssImport from "postcss-import";
-import neatgrid from "postcss-neat";
 import nestedcss from "postcss-nested";
-import colorfunctions from "postcss-colour-functions";
-import hdBackgrounds from "postcss-at2x";
-import cssextend from "postcss-simple-extend";
 import BrowserSync from "browser-sync";
 import webpack from "webpack";
 import webpackConfig from "./webpack.conf";
@@ -16,54 +12,62 @@ const browserSync = BrowserSync.create();
 const hugoBin = "hugo";
 const defaultArgs = ["-d", "../dist", "-s", "site", "-v"];
 
-gulp.task("hugo", (cb) => buildSite(cb));
-gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
+gulp.task("hugo", cb => buildSite(cb));
+gulp.task("hugo-preview", cb =>
+  buildSite(cb, ["--buildDrafts", "--buildFuture"])
+);
 
 gulp.task("build", ["css", "js", "videos", "images", "hugo"]);
 gulp.task("build-preview", ["css", "js", "videos", "images", "hugo-preview"]);
 
-gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
-    .pipe(postcss([
-      cssImport({from: "./src/css/main.css"}),
-      neatgrid(),
-      colorfunctions(),
-      nestedcss(),
-      hdBackgrounds(),
-      cssextend()]))
+gulp.task("css", () =>
+  gulp
+    .src("./src/css/*.css")
+    .pipe(postcss([cssImport({ from: "./src/css/main.css" }), nestedcss()]))
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
-));
+);
 
-gulp.task("js", (cb) => {
+gulp.task("js", cb => {
   const myConfig = Object.assign({}, webpackConfig);
 
   webpack(myConfig, (err, stats) => {
     if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
+    gutil.log(
+      "[webpack]",
+      stats.toString({
+        colors: true,
+        progress: true
+      })
+    );
     browserSync.reload();
     cb();
   });
 
-  gulp.src(["./src/js/**/*", "!./src/js/app.js", "!./src/js/cms.js", "!./src/js/cms/**/*"])
+  gulp
+    .src([
+      "./src/js/**/*",
+      "!./src/js/app.js",
+      "!./src/js/cms.js",
+      "!./src/js/cms/**/*"
+    ])
     .pipe(gulp.dest("./dist/js"))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
 });
 
-gulp.task("videos", () => (
-  gulp.src("./src/videos/**/*")
+gulp.task("videos", () =>
+  gulp
+    .src("./src/videos/**/*")
     .pipe(gulp.dest("./dist/videos"))
     .pipe(browserSync.stream())
-));
+);
 
-gulp.task("images", () => (
-  gulp.src("./src/img/**/*")
+gulp.task("images", () =>
+  gulp
+    .src("./src/img/**/*")
     .pipe(gulp.dest("./dist/img"))
     .pipe(browserSync.stream())
-));
+);
 
 gulp.task("server", ["hugo", "css", "js", "videos", "images"], () => {
   browserSync.init({
@@ -82,7 +86,7 @@ gulp.task("server", ["hugo", "css", "js", "videos", "images"], () => {
 function buildSite(cb, options) {
   const args = options ? defaultArgs.concat(options) : defaultArgs;
 
-  return cp.spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
+  return cp.spawn(hugoBin, args, { stdio: "inherit" }).on("close", code => {
     if (code === 0) {
       browserSync.reload();
       cb();
